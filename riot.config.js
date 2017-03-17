7d571522bfe63b476cbf0a4a7a88b8f1bb198590
rainspr <rainspr@icloud.com>
2017-03-17 16:00:44 +0900

    rollupは後で

---
+++ riot.config.js
@@ -0,0 +1,10 @@
+import buble from 'buble'
+import pug from 'pug'
+
+export default {
+  from: 'src',
+  to: 'dist/bundle.js',
+  ext: 'pug',
+  template: 'pug',
+  type: 'buble'
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import riot from 'riot'
+import route from 'riot-route'
 import './app.tag.pug'
+import './pray.tag.pug'
+import './gp.tag.pug'
 
-riot.mount('app')
+riot.compile(function() {
+  riot.mount('*')
+  route.start(true)
+})
